03046642e25735d77fbfe7ed6d744019e3467c7e
midoks <midoks@163.com>
2017-12-15 19:26:16 +0800

    开发保存

--- MDserver/WebSite.Designer.cs
+++ MDserver/WebSite.Designer.cs
@@ -29,13 +29,123 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WebSite));
+            this.domainList = new System.Windows.Forms.DataGridView();
+            this.domain = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.port = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupList = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.domainList)).BeginInit();
+            this.groupList.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // domainList
+            // 
+            this.domainList.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.domainList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.domainList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.domainList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.domain,
+            this.port});
+            this.domainList.GridColor = System.Drawing.SystemColors.ButtonFace;
+            this.domainList.Location = new System.Drawing.Point(6, 20);
+            this.domainList.Name = "domainList";
+            this.domainList.RowTemplate.Height = 23;
+            this.domainList.Size = new System.Drawing.Size(246, 216);
+            this.domainList.TabIndex = 0;
+            // 
+            // domain
+            // 
+            this.domain.HeaderText = "域名";
+            this.domain.Name = "domain";
+            // 
+            // port
+            // 
+            this.port.HeaderText = "端口";
+            this.port.MaxInputLength = 10;
+            this.port.Name = "port";
+            // 
+            // groupList
+            // 
+            this.groupList.Controls.Add(this.domainList);
+            this.groupList.Location = new System.Drawing.Point(12, 12);
+            this.groupList.Name = "groupList";
+            this.groupList.Size = new System.Drawing.Size(262, 257);
+            this.groupList.TabIndex = 1;
+            this.groupList.TabStop = false;
+            this.groupList.Text = "列表";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(284, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(259, 257);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "操作";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(197, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(91, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(65, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port Number:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server Name:";
             // 
             // WebSite
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(563, 296);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupList);
+            this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -43,10 +153,25 @@
             this.Name = "WebSite";
             this.Text = "WebSite";
             this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.domainList)).EndInit();
+            this.groupList.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView domainList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn domain;
+        private System.Windows.Forms.DataGridViewTextBoxColumn port;
+        private System.Windows.Forms.GroupBox groupList;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
